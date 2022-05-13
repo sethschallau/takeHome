35dd8806a0b11e8bb0595020c176785b84268ef7
Seth <sethmschallau@gmail.com>
2022-05-12 21:44:30 -0400

    built out flask app, need to finish styling

---
+++ app.py
@@ -0,0 +1,49 @@
+import sqlite3
+import os
+import statistics
+
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+def get_db_connection():
+    conn = sqlite3.connect('fission.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def index_post():
+
+    zipcode = int(request.form['text'])
+
+    conn = get_db_connection()
+    query = (
+        "SELECT pricing.price, pricing.area, "
+               "features.bed, features.bath, "
+               "address, city, zipcode FROM location "
+        "INNER JOIN pricing USING (id) "
+        "INNER JOIN features USING (id) "
+        f"WHERE zipcode = {zipcode} "
+        "LIMIT 10;"
+    )
+    posts = conn.execute(query).fetchall()
+    conn.close()
+    
+    prices = []
+    for listing in posts:
+        prices.append(listing['price'])
+    
+    prices.sort()
+    stats_dict = {
+        "low" : prices[0],
+        "high" : prices[-1],
+        "mean" : statistics.mean(prices),
+        "std" : statistics.stdev(prices) // 1
+    }
+
+    return render_template('loop.html', listings=posts, stats=stats_dict)
